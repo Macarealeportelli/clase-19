49a04995e7a43921827c423c7a7f5d0e8901adc1
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-08-26 20:36:09 -0300

    ahora si funca

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const cancion1 = prompt ("Decime una canción")
-const cancion2 = prompt ("Decime otra canción")
-const cancion3 = prompt ("Vamos bien! Decime otra canción")
-const cancion4 = prompt ("Ya casi terminamos, decime otra canción")
-const cancion5 = prompt ("Y llegamos! Decime una última canción")
-
 const top1 = document.getElementById ("cancion1") 
 top1.textContent = cancion1
 
+const cancion2 = prompt ("Decime otra canción")
 const top2 = document.getElementById ("cancion2") 
-top1.textContent = cancion2
+top2.textContent = cancion2
 
+const cancion3 = prompt ("Vamos bien! Decime otra canción")
 const top3 = document.getElementById ("cancion3") 
-top1.textContent = cancion3
+top3.textContent = cancion3
 
+const cancion4 = prompt ("Ya casi terminamos, decime otra canción")
 const top4 = document.getElementById ("cancion4") 
-top1.textContent = cancion4
+top4.textContent = cancion4
 
+const cancion5 = prompt ("Y llegamos! Decime una última canción")
 const top5 = document.getElementById ("cancion5") 
-top1.textContent = cancion5
+top5.textContent = cancion5

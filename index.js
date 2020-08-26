7d46eab5653c008b1c2d59f3fefff584977ad30b
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-08-26 20:31:30 -0300

    practica con Dom

---
+++ index.js
@@ -0,0 +1,20 @@
+const cancion1 = prompt ("Decime una canción")
+const cancion2 = prompt ("Decime otra canción")
+const cancion3 = prompt ("Vamos bien! Decime otra canción")
+const cancion4 = prompt ("Ya casi terminamos, decime otra canción")
+const cancion5 = prompt ("Y llegamos! Decime una última canción")
+
+const top1 = document.getElementById ("cancion1") 
+top1.textContent = cancion1
+
+const top2 = document.getElementById ("cancion2") 
+top1.textContent = cancion2
+
+const top3 = document.getElementById ("cancion3") 
+top1.textContent = cancion3
+
+const top4 = document.getElementById ("cancion4") 
+top1.textContent = cancion4
+
+const top5 = document.getElementById ("cancion5") 
+top1.textContent = cancion5
